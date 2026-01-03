015d3adc08906f0772f38652a166f0ff9dc5fa3a
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-03 01:35:04 -0800

    Add basic site routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return 'About Page';
+})->name('about');
+
+
+Route::get('/projects', function () {
+    return 'Projects Page';
+})->name('projects');
+
+Route::get('/contact', function () {
+    return 'Contact Page';
+})->name('contact');
+
+
